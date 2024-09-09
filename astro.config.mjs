a4a3159307d0a13ec8a8e37d65ccc9a1b475e31f
Cristian Chitiva <cychitivav@unal.edu.co>
2024-09-09 18:53:07 +0000

    fix: Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://crisitanchitiva.dev",
+	site: "https://cristianchitiva.dev",
 	base: "/",
 })
